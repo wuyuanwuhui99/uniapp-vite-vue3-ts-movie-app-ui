4587f3f39c5e6302aaa15a78c18cffba92fbd933
wuyuanwuhui99 <275018723@qq.com>
2024-08-12 23:31:51 +0800

    代码格式化

--- src/music/pages/MusicClassifyListPage.vue
+++ src/music/pages/MusicClassifyListPage.vue
@@ -1,12 +1,13 @@
 <template>
 	<view class="page-wrapper" @scrolltolower="onScrolltolower" scroll-y show-scrollbar="false">
 		<view class="page-header">
-			<image class="icon-back" @click="useBack" src="../../../static/icon_back.png"/>
+			<image class="icon-back" @click="useBack" src="../../../static/icon_back.png" />
 			<text class="my-favorite">我的收藏夹</text>
-			<view class="icon-back"/>
+			<view class="icon-back" />
 		</view>
-		<scroll-view  scroll-y show-scrollbar="false" class="page-body">
-			<MusicClassifyListComponent @on-play-music="usePlayMusic" :music-list="musicList" :classify-name="musicClassify.classifyName"/>
+		<scroll-view scroll-y show-scrollbar="false" class="page-body">
+			<MusicClassifyListComponent @on-play-music="usePlayMusic" :music-list="musicList"
+				:classify-name="musicClassify.classifyName" />
 
 			<text class="footer">{{ total > pageNum * PAGE_SIZE ? '正在加载更多' : '已经到底了'}}</text>
 		</scroll-view>
@@ -14,21 +15,21 @@
 </template>
 
 <script setup lang="ts">
-	import { ref,defineProps, reactive } from 'vue';
+	import { ref, defineProps, reactive } from 'vue';
 	import { useRoute } from "vue-router";
-	import type { MusicType,MusicClassifyType } from '../types';
+	import type { MusicType, MusicClassifyType } from '../types';
 	import { getMusicListByClassifyIdService } from '../service';
-	import { MAX_FAVORITE_NUMBER,PAGE_SIZE } from '../../common/constant';
+	import { MAX_FAVORITE_NUMBER, PAGE_SIZE } from '../../common/constant';
 	import { useStore } from "../../stores/useStore";;
 	import MusicClassifyListComponent from '../components/MusicClassifyListComponent.vue';
 
 	const store = useStore();
 	const route = useRoute();
 
-	let loadding:boolean = false;
+	let loadding : boolean = false;
 	const total = ref<number>(0);
 	const pageNum = ref<number>(1);
-	const musicClassify:MusicClassifyType = JSON.parse(decodeURIComponent(route.query.data as string)) as MusicClassifyType; // 获取URL上的查询参数并反序列化
+	const musicClassify : MusicClassifyType = JSON.parse(decodeURIComponent(route.query.data as string)) as MusicClassifyType; // 获取URL上的查询参数并反序列化
 	const musicList = reactive<Array<MusicType>>([]);
 
 	/**
@@ -36,32 +37,32 @@
 	 * @date: 2024-07-21 14:31
 	 * @author wuwenqiang
 	 */
-	const useMusicList = () =>{
-		getMusicListByClassifyIdService(musicClassify.id,pageNum.value,PAGE_SIZE).then((res)=>{
+	const useMusicList = () => {
+		getMusicListByClassifyIdService(musicClassify.id, pageNum.value, PAGE_SIZE).then((res) => {
 			total.value = res.total;
 			musicList.push(...res.data);
-		}).finally(()=>loadding = false);
+		}).finally(() => loadding = false);
 	}
-	
-	
+
+
 	/**
 	 * @description: 返回按钮
 	 * @date: 2024-05-10 22:10
 	 * @author wuwenqiang
 	 */
-	const useBack = ()=>{
+	const useBack = () => {
 		uni.navigateBack()
 	}
-	
-	const usePlayMusic = async(item:MusicType,index:number)=>{
-		if(store.classifyName != musicClassify.classifyName){
-			await getMusicListByClassifyIdService(musicClassify.id, 1, MAX_FAVORITE_NUMBER).then((value) =>{
-				store.setClassifyMusic(value.data,item,index,musicClassify.classifyName);
+
+	const usePlayMusic = async (item : MusicType, index : number) => {
+		if (store.classifyName != musicClassify.classifyName) {
+			await getMusicListByClassifyIdService(musicClassify.id, 1, MAX_FAVORITE_NUMBER).then((value) => {
+				store.setClassifyMusic(value.data, item, index, musicClassify.classifyName);
 			});
-		}else{
-			store.setMusic(item,true);
+		} else {
+			store.setMusic(item, true);
 		}
-		uni.navigateTo({url: `../pages/MusicPlayerPage`});
+		uni.navigateTo({ url: `../pages/MusicPlayerPage` });
 	}
 
 	/**
@@ -69,7 +70,7 @@
 	 * @date: 2024-05-10 22:10
 	 * @author wuwenqiang
 	 */
-	const onScrolltolower = ()=>{
+	const onScrolltolower = () => {
 		if (loadding) return;
 		if (total.value > PAGE_SIZE * pageNum.value) {
 			pageNum.value++;
@@ -92,7 +93,7 @@
 			justify-content: space-between;
 			background: @module-background-color;
 
-			.icon-back{
+			.icon-back {
 				width: @small-icon-size;
 				height: @small-icon-size;
 			}
@@ -101,40 +102,45 @@
 		.page-body {
 			flex: 1;
 			overflow: hidden;
-			padding:0 @page-padding 0;
+			padding: 0 @page-padding 0;
 			box-sizing: border-box;
-			.module-block{
+
+			.module-block {
 				align-items: center;
-				gap:  @page-padding;
-				.favorite-cover{
+				gap: @page-padding;
+
+				.favorite-cover {
 					width: @big-avater;
 					height: @big-avater;
 					border-radius: @module-border-radius;
 				}
 
-				.favorite-name-wrapper{
+				.favorite-name-wrapper {
 					display: flex;
 					flex-direction: column;
 					gap: @page-padding;
 					flex: 1;
-					.favorite-total{
-						color:@disable-text-color;
+
+					.favorite-total {
+						color: @disable-text-color;
 					}
 				}
 			}
-			
-			.music-row{
+
+			.music-row {
 				width: 100%;
 				display: flex;
 				gap: @page-padding;
 				align-items: center;
 				padding-bottom: @page-padding;
 				border-bottom: 1rpx solid @disable-text-color;
-				&:last-child{
-					border-bottom:none;
-					padding-bottom:0;
+
+				&:last-child {
+					border-bottom: none;
+					padding-bottom: 0;
 				}
-				.music-name{
+
+				.music-name {
 					flex: 1;
 					width: 0;
 					overflow: hidden;
@@ -143,7 +149,7 @@
 				}
 			}
 
-			.footer{
+			.footer {
 				width: 100%;
 				padding: @page-padding;
 				text-align: center;
